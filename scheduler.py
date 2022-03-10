3a0155ae18e089c8f57c555af0eeedb64c9d8358
minhtrannhat <minhtrannhat@tutanota.com>
2022-03-09 19:56:12 -0500

    Feat: added helper functions for the scheduler
    - adding processes into either expired or active queue.

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,25 @@
+from queue import PriorityQueue
+
+
 class Scheduler:
     schedulerTotalProcessesQueue = []
-    schedulerQ1 = []
-    schedulerQ2 = []
+    schedulerQ1 = PriorityQueue()
+    schedulerQ2 = PriorityQueue()
     numberOfProcesses: int = 0
+
+    activeQueue = schedulerQ1
+    expiredQueue = schedulerQ2
 
     def __init__(self) -> None:
         pass
+
+    def switchFlagsOfQueues(self) -> None:
+        tmp = self.activeQueue
+        self.activeQueue = self.expiredQueue
+        self.expiredQueue = tmp
+
+    def insertIntoExpiredQueue(self, schedulerProcess) -> None:
+        self.expiredQueue.put(schedulerProcess.priority, schedulerProcess.PID)
+
+    def insertIntoActiveQueue(self, schedulerProcess) -> None:
+        self.activeQueue.put(schedulerProcess.priority, schedulerProcess.PID)
