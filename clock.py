221dcdd8c454804c764e298470e1114b8fc5dafd
minhtrannhat <minhtrannhat@tutanota.com>
2022-03-10 20:46:59 -0500

    Feat: Partial implementation of the Clock thread

--- clock.py
+++ clock.py
@@ -1,6 +1,15 @@
 from threading import Thread
+from time import sleep
 
 
 class Clock(Thread):
     def __init__(self):
         super(Clock, self).__init__()
+        self.currentTime: int = 0
+
+    def run(self):
+        while True:
+            # sleep for 50 milliseconds
+            sleep(0.05)
+            # increment clock by 5 milliseconds
+            self.currentTime = self.currentTime + 5
